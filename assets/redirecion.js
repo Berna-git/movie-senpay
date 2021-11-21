78298fd793c8291ad833697a76c4bac3b4d22bc2
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-11-20 21:31:32 -0300

    litle correction

--- assets/redirecion.js
+++ assets/redirecion.js
@@ -1,10 +1,4 @@
-// Traigo el boton del login y redirecciono al home
-// window.location.href = "https://professor-falken.com";
-
-const btnRedirection = document.getElementById('btn-redirection-home');
-btnRedirection.addEventListener('click',redirection )
-// windows.location.href = 'http://url.com';
-
+// redirection to home
 function redirection() {
     location.href = "../home.html"
 }
